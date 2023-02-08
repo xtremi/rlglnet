c4e848a93a227f241d8766cf9ddc5d32dcf6c2b3
remi <remilanza@gmail.com>
2023-02-08 22:22:37 +0100

    working on quad tree

--- rlglnet/rlglQuadTree.cs
+++ rlglnet/rlglQuadTree.cs
@@ -1,40 +1,63 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace rlglnet
 {
 
-    class rlglQuadTreeElement
-    {
+    class rlglQuadTreeElement : IEnumerator { 
         GlmNet.vec3 _center;
         int _level;
         rlglQuadTree _quadTree;
+
         public rlglQuadTreeElement(rlglQuadTree quadTree, GlmNet.vec3 center, int level)
         {
             _quadTree = quadTree;
             _center = center;
             _level = level;
         }
-
+        public float size()
+        {
+            return _quadTree._totalSize / (float)(_level + 1);
+        }
         protected rlglQuadTreeElement[] children = null;
+
+
         public bool isSplit(){
             return children != null; 
         }
         public void split()
         {
-            float size = _quadTree._totalSize / (float)(_level + 2);
+            float childrenSize = size() / 2.0f;
             children = new rlglQuadTreeElement[4]
             {
-                new rlglQuadTreeElement(_quadTree, _center + size * new GlmNet.vec3(-1.0f, -1.0f, 0.0f), _level + 1),
-                new rlglQuadTreeElement(_quadTree, _center + size * new GlmNet.vec3( 1.0f, -1.0f, 0.0f), _level + 1),
-                new rlglQuadTreeElement(_quadTree, _center + size * new GlmNet.vec3( 1.0f,  1.0f, 0.0f), _level + 1),
-                new rlglQuadTreeElement(_quadTree, _center + size * new GlmNet.vec3(-1.0f,  1.0f, 0.0f), _level + 1)
+                new rlglQuadTreeElement(_quadTree, _center + childrenSize * new GlmNet.vec3(-1.0f, -1.0f, 0.0f), _level + 1),
+                new rlglQuadTreeElement(_quadTree, _center + childrenSize * new GlmNet.vec3( 1.0f, -1.0f, 0.0f), _level + 1),
+                new rlglQuadTreeElement(_quadTree, _center + childrenSize * new GlmNet.vec3( 1.0f,  1.0f, 0.0f), _level + 1),
+                new rlglQuadTreeElement(_quadTree, _center + childrenSize * new GlmNet.vec3(-1.0f,  1.0f, 0.0f), _level + 1)
             };
         }
 
+
+        rlglQuadTreeElement child(uint i)
+        {
+            return children == null || i > 3 ? null : children[i];
+        }
+
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
     }
-    class rlglQuadTree
+    class rlglQuadTree : IEnumerable<rlglQuadTreeElement>
     {
         rlglQuadTree(float totalSize, int _maxSubdivision)
         {
@@ -49,6 +72,14 @@ namespace rlglnet
 
         rlglQuadTreeElement root;
 
+        public IEnumerator<rlglQuadTreeElement> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
