df7ba1769b7a48e872d9b6c813ba8d3c00416be1
Remi Lanza <remi.lanza@jotne.com>
2023-02-13 08:39:14 +0100

    started rlglrenderer

---
+++ rlglnet/rlglRenderer.cs
@@ -0,0 +1,27 @@
+﻿
+namespace rlglnet
+{
+    public class rlglRenderer
+    {
+        public void Render(List<rlglRenderableObject> objects)
+        {
+            foreach (rlglRenderableObject obj in objects)
+            {
+                Render(obj);
+            }
+        }
+
+        protected void Render(rlglRenderableObject obj)
+        {
+
+
+
+
+        }
+
+
+        rlglShader previousShader;
+        rlglMesh previousMesh;
+
+    }
+}
